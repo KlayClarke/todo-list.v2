094ed499719815d511ef02532e450f608ac2cb5e
Klay Clarke <klayaclarke@gmail.com>
2022-02-15 12:56:51 -0500

    add delete button to each project when added to dom - give each project div an id based on projects id - give each delete button an id based on projects div id

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
-import { Project, storeProject } from "./factory";
 import "./storage";
-import "./modal";
+import { Project } from "./factory";
 
 const createProjectButton = document.querySelector("#create-project");
 
@@ -20,6 +19,7 @@ function createProject() {
   document.querySelector("textarea[name='project-description']").value = "";
   document.querySelector("input[name='project-due-date']").value = "";
   displayProjects();
+  displayTodos();
 }
 
 function displayProjects() {
@@ -27,14 +27,40 @@ function displayProjects() {
   projects.innerHTML = "";
   if (JSON.parse(localStorage.getItem("projects"))) {
     for (let project of JSON.parse(localStorage.getItem("projects"))) {
+      // create div to hold project
       let projectDiv = document.createElement("div");
       projectDiv.classList.add("project");
+      projectDiv.setAttribute("id", project.id);
       projectDiv.innerText = `${project.name}: ${project.description} | ${project.dueDate}`;
+      // add delete button to project div
+      addDeleteButton(projectDiv);
+      // append newly created project to div of all projects
       projects.appendChild(projectDiv);
+      // create div to hold todos in project
+      let todosDiv = document.createElement("div");
+      todosDiv.classList.add("todos");
+      projectDiv.appendChild(todosDiv);
+    }
+  }
+}
+
+function addDeleteButton(el) {
+  let deleteButton = document.createElement("span");
+  deleteButton.classList.add("delete");
+  deleteButton.setAttribute("id", el.id);
+  deleteButton.innerHTML = "&times;";
+  el.appendChild(deleteButton);
+}
+
+function displayTodos() {
+  if (document.querySelectorAll(".todo")) {
+    for (let todo of document.querySelectorAll(".todo")) {
+      console.log(todo);
     }
   }
 }
 
 displayProjects();
+displayTodos();
 
 createProjectButton.addEventListener("click", createProject);

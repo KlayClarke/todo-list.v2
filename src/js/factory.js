da1f052174ea0a0b25671471ed7b75dcdb6ed8a4
Klay Clarke <klayaclarke@gmail.com>
2022-02-15 11:19:27 -0500

    edit project factory function - add project store method that stores project on local storage

--- src/js/factory.js
+++ src/js/factory.js
@@ -1,20 +1,42 @@
-export function project(name, description, dueDate) {
-  let todos = [];
+import { storageAvailable } from "./storage";
 
-  function addTodo(name, priority) {
+export function Project(name, description, dueDate) {
+  // create project object
+  let project = {};
+
+  // parameters as keys
+  project.name = name;
+  project.description = description;
+  project.dueDate = dueDate;
+  project.todos = [];
+
+  // function to add todo to project
+  project.addTodo = function (name, priority) {
     let todo = {
       id: Date.now(),
       name: name,
       priority: priority,
     };
-    todos.push(todo);
-  }
-
-  return {
-    name,
-    description,
-    dueDate,
-    todos,
-    addTodo,
+    project.todos.push(todo);
   };
+
+  // function to store project in local storage
+  project.store = function () {
+    if (storageAvailable("localStorage")) {
+      // if no projects instance on local storage, create one
+      if (!localStorage.getItem("projects")) {
+        localStorage.setItem("projects", JSON.stringify([]));
+      }
+      // parse projects instance - turn JSON back to js object
+      let projects = JSON.parse(localStorage.getItem("projects"));
+      // add newly created project to projects instance - saved as new variable
+      projects.push(project);
+      // set local storage projects instance as object with newly added project
+      localStorage.setItem("projects", JSON.stringify(projects));
+    }
+  };
+
+  return project;
 }
+
+// want to store project, but also want to be able to update storage every time a project adds todos

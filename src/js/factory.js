71af3330a6936f2f00fe971168f12198ea7dfef8
Klay Clarke <klayaclarke@gmail.com>
2022-02-15 23:04:55 -0500

    remove project factory function - have user init function instead that houses object with projects, todos and methods for crud

--- src/js/factory.js
+++ src/js/factory.js
@@ -1,35 +1,38 @@
 import { storageAvailable } from "./storage";
 
-export function Project(name, description, dueDate) {
-  // create project object
-  let project = {};
+export function initializeUser() {
+  let userObject = {};
 
-  // parameters as keys
-  project.id = Date.now();
-  project.name = name;
-  project.description = description;
-  project.dueDate = dueDate;
-  project.todos = [];
+  userObject.projects = [];
 
-  // function to store project in local storage
-  function storeProject() {
-    if (storageAvailable("localStorage")) {
-      // if no projects instance on local storage, create one
-      if (!localStorage.getItem("projects")) {
-        localStorage.setItem("projects", JSON.stringify([]));
+  userObject.project = function (name, description = "", dueDate) {
+    let project = {};
+
+    project.id = Date.now();
+    project.name = name;
+    project.description = description;
+    project.dueDate = dueDate;
+    project.todos = [];
+
+    userObject.projects.push(project);
+  };
+
+  userObject.todo = function (projectId, name) {
+    let todo = {};
+
+    todo.id = Date.now();
+    todo.name = name;
+
+    for (let p of userObject.projects) {
+      if (p.id == projectId) {
+        p.todos.push(todo);
       }
-      // parse projects instance - turn JSON back to js object
-      let projects = JSON.parse(localStorage.getItem("projects"));
-      // add newly created project to projects instance - saved as new variable
-      projects.push(project);
-      // set local storage projects instance as object with newly added project
-      localStorage.setItem("projects", JSON.stringify(projects));
     }
-  }
-  // store project when project is created
-  storeProject();
 
-  return Object.freeze(project);
+    //   [projectId].push(todo);
+  };
+
+  return userObject;
 }
 
 // allow user to hover over a project to reveal a form to allow them to add todo to said project / and projects todos

e483095f6c7b681c0c8d6202f0066dc800cb7a54
Klay Clarke <klayaclarke@gmail.com>
2022-02-16 22:50:16 -0500

    refactor

--- src/js/updateDOM.js
+++ src/js/updateDOM.js
@@ -1,52 +1,5 @@
 let projects = document.querySelector("#projects");
 
-function displayTodos() {
-  projects.innerHTML = "";
-  if (JSON.parse(localStorage.getItem("projects"))) {
-    for (let project of JSON.parse(localStorage.getItem("projects"))) {
-      let todosDiv = document.createElement("div");
-      todosDiv.classList.add("todos");
-      todosDiv.setAttribute("id", project.id);
-      projects.appendChild(todosDiv);
-      for (let todo of project.todos) {
-        let todoDiv = document.createElement("div");
-        todoDiv.classList.add("todo");
-        todoDiv.setAttribute("id", todo.id);
-        addInfoParagraphForTodo(todo, todoDiv);
-        todosDiv.appendChild(todoDiv);
-      }
-    }
-  }
-}
-
-function displayProjects() {
-  if (JSON.parse(localStorage.getItem("projects"))) {
-    for (let project of JSON.parse(localStorage.getItem("projects"))) {
-      // create project div to hold project
-      let projectDiv = document.createElement("div");
-      projectDiv.classList.add("project");
-      projectDiv.setAttribute("id", project.id);
-      // create paragraph with project info
-      addInfoParagraphForProject(project, projectDiv);
-      // add todo creation form to project
-      addTodoForm(projectDiv);
-      // add project delete button to project
-      addDeleteButton(projectDiv);
-      // append projectDiv to project's section before project's corresponding todo div (same ids)
-      for (let todosDiv of document.querySelectorAll(".todos")) {
-        if (todosDiv.id == projectDiv.id) {
-          projects.insertBefore(projectDiv, todosDiv);
-        }
-      }
-    }
-  }
-}
-
-export function updateDOM() {
-  displayTodos();
-  displayProjects();
-}
-
 function addInfoParagraphForProject(project, parentDiv) {
   let paragragh = document.createElement("p");
   paragragh.classList.add("project-info");
@@ -91,4 +44,61 @@ function addDeleteButton(parentDiv) {
   deleteButton.setAttribute("type", "submit");
   deleteButton.innerHTML = "&times;";
   parentDiv.appendChild(deleteButton);
+}
+
+function addCheckBox(todo, parentDiv) {
+  let checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.setAttribute("name", "checkbox");
+  checkBox.setAttribute("id", todo.id);
+  parentDiv.appendChild(checkBox);
+}
+
+function displayTodos() {
+  projects.innerHTML = "";
+  if (JSON.parse(localStorage.getItem("projects"))) {
+    for (let project of JSON.parse(localStorage.getItem("projects"))) {
+      let todosDiv = document.createElement("div");
+      todosDiv.classList.add("todos");
+      todosDiv.setAttribute("id", project.id);
+      projects.appendChild(todosDiv);
+      for (let todo of project.todos) {
+        let todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+        todoDiv.setAttribute("id", todo.id);
+        addInfoParagraphForTodo(todo, todoDiv);
+        addCheckBox(todo, todoDiv);
+        todosDiv.appendChild(todoDiv);
+      }
+    }
+  }
+}
+
+function displayProjects() {
+  if (JSON.parse(localStorage.getItem("projects"))) {
+    for (let project of JSON.parse(localStorage.getItem("projects"))) {
+      // create project div to hold project
+      let projectDiv = document.createElement("div");
+      projectDiv.classList.add("project");
+      projectDiv.setAttribute("id", project.id);
+      // create paragraph with project info
+      addInfoParagraphForProject(project, projectDiv);
+      // add todo creation form to project
+      addTodoForm(projectDiv);
+      // add project delete button to project
+      addDeleteButton(projectDiv);
+      // append projectDiv to project's section before project's corresponding todo div (same ids)
+      for (let todosDiv of document.querySelectorAll(".todos")) {
+        if (todosDiv.id == projectDiv.id) {
+          projects.insertBefore(projectDiv, todosDiv);
+        }
+      }
+    }
+  }
+}
+
+export function updateDOM() {
+  displayTodos();
+  displayProjects();
+  console.log(JSON.parse(localStorage.getItem("projects")));
 }

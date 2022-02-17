5f30fc6bb77b99f360300d47514bc40e0c1c5d6b
Klay Clarke <klayaclarke@gmail.com>
2022-02-17 09:09:27 -0500

    update imports

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,6 @@
-import "./updateDom";
+import "./modal";
+import "./updateDOM";
 import "./updateStorage";
 
 // todos:
-// - professional, sophisticated, intellectual, Apple-esque styles
-// - refactor code - organize
-
-// cool functionality:
-// - make projects orderable by drag and drop (allow user to drag 2nd todo above first to change their order in user.projects)
+// - professional, sleek, sophisticated, intellectual, Apple-esque styles
